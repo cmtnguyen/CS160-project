4c9d9279acac37f358918be7a3fb32182655fd42
Catherine Nguyen <catherine.nguyen01@sjsu.edu>
2022-04-07 01:24:24 -0700

    add user check to view reservations

--- src/Components/Pages/LoginPage/LoginPage.jsx
+++ src/Components/Pages/LoginPage/LoginPage.jsx
@@ -2,7 +2,6 @@ import { useEffect, Fragment } from "react";
 import { useNavigate } from "react-router-dom";
 import { auth, signInWithGoogle } from "../../../firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
-import Navbar from "../../Navbar";
 import styles from "./LoginPage.module.css";
 function Login() {
   const [user, loading] = useAuthState(auth);
@@ -12,11 +11,10 @@ function Login() {
       // maybe trigger a loading screen
       return;
     }
-    if (user) navigate("/reserve");
+    if (user) navigate("/reservations");
   }, [user, loading, navigate]);
   return (
     <Fragment>
-      <Navbar />
       <div className={styles.login}>
         <h1>Login with Google to Reserve a Spot!</h1>
         <button className={styles.loginBtn} onClick={signInWithGoogle}>
--- src/Components/Pages/ViewPage/ViewPage.jsx
+++ src/Components/Pages/ViewPage/ViewPage.jsx
@@ -1,12 +1,21 @@
 import { Fragment } from "react";
 import { Row, Container } from "react-bootstrap";
-import Navbar from "../../Navbar";
+import { useEffect } from "react";
+import { auth } from "../../../firebase";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useNavigate } from "react-router-dom";
 import styles from "./ViewPage.module.css";
 
 const ViewPage = () => {
+  const [user, loading] = useAuthState(auth);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (loading) return;
+    if (!user) return navigate("/login");
+  }, [user, loading, navigate]);
   return (
     <Fragment>
-      <Navbar />
       <Container className={styles.checkAlign}>
         <h1 className={styles.checkTitle}>Check In</h1>
         <Row className={styles.columnGap}>
